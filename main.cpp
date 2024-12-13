0de5aff6f1c21c5a35fc55e51d7ef67160f0cfb0
nnzzll <nnzzll@zju.edu.cn>
2024-12-13 18:59:48 +0800

    Init

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "BayesOpt/Kernel"
+#include "BayesOpt/GaussianProcess"
+#include "BayesOpt/AcquisitionStrategy"
+
+int main(int argc, char** argv)
+{
+  RBFKernel kernel;
+  return 0;
+}
