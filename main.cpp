826069529d6067bb64ff37067e9f3b8f49608ef6
root <root@LAPTOP-K5S9KB4T.localdomain>
2024-12-17 11:52:56 +0800

    FIX:修复Acquisition的问题, 测试通过

--- main.cpp
+++ main.cpp
@@ -22,12 +22,12 @@ int main(int argc, char** argv)
 {
   RBFKernel kernel;
   GaussianProcess<RBFKernel> gp(kernel, 1e-5);
-  UpperConfidenceBound<GaussianProcess<RBFKernel>> strategy;
+  LowerConfidenceBound<GaussianProcess<RBFKernel>> strategy;
   Rosenbrock func;
   std::vector<Eigen::Vector2d> bounds{{-2.0, 2.0}, {-2.0, 2.0}};
-  BayesianOptimization<GaussianProcess<RBFKernel>, UpperConfidenceBound<GaussianProcess<RBFKernel>>>
-      opt(gp, strategy, bounds, 100, 200, 100);
-  Eigen::Vector2d ans = opt.optimize<Rosenbrock>(func, Eigen::Vector2d(1.7, -0.5), true);
+  BayesianOptimization<GaussianProcess<RBFKernel>, LowerConfidenceBound<GaussianProcess<RBFKernel>>>
+      opt(gp, strategy, bounds, 100, 200);
+  Eigen::Vector2d ans = opt.optimize<Rosenbrock>(func, true);
   std::cout << "Ans:" << ans.transpose() << std::endl;
   return 0;
 }
